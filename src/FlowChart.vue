6db11a5fee9868f1001de6530db0bd20566a6b4c
Nightr0ck <leequan2000@gmail.com>
2024-11-25 01:17:18 +0800

    create trigger node, add trigger node to vueflow component

--- src/FlowChart.vue
+++ src/FlowChart.vue
@@ -1,7 +1,45 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue';
+import { VueFlow, useVueFlow } from '@vue-flow/core';
+import { Background } from '@vue-flow/background';
+import '@vue-flow/core/dist/style.css';
+import '@vue-flow/core/dist/theme-default.css';
+import TriggerNode from './components/vue-flow components/TriggerNode.vue';
 
-const counter = ref(0);
+const nodes = ref([
+  {
+    id: '1',
+    type: 'input',
+    position: { x: 250, y: 5 },
+    data: { label: 'Node 1' },
+  },
+  {
+    id: '2',
+    position: { x: 100, y: 100 },
+    data: { label: 'Node 2' },
+  },
+  {
+    id: '3',
+    type: 'trigger',
+    position: { x: 400, y: 200 },
+    data: { label: 'Node 3' },
+  },
+]);
+const edges = ref([
+  {
+    id: 'e1->2',
+    source: '1',
+    target: '2',
+    animated: true,
+  }
+]);
+
+const { addEdges } = useVueFlow();
+
+function onConnect(connection) {
+  console.log("on connect", connection);
+  addEdges(connection);
+}
 
 onMounted(() => {
   console.log("flowchart mounted");
@@ -10,10 +48,25 @@ onMounted(() => {
 </script>
 
 <template>
-  Hello World
-  {{ counter }}
+  <div class="flow-chart-main">
+    <VueFlow class="vue-flow-component" :nodes="nodes" :edges="edges" @connect="onConnect">
+      <Background patternColor="black" />
+
+      <template #node-trigger="triggerNodeProps">
+        <TriggerNode v-bind="triggerNodeProps" />
+      </template>
+
+    </VueFlow>
+  </div>
 </template>
 
 <style scoped>
+.flow-chart-main {
+  height: 1000px;
+  width: 1000px;
 
+  .vue-flow-component {
+    background-color: #f2f5f3;
+  }
+}
 </style>
---
+++ src/components/vue-flow components/TriggerNode.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import NodeCard from './NodeCard.vue';
+  import { Handle, Position } from '@vue-flow/core'
+</script>
+
+<template>
+  <NodeCard headerIconSrc="/trigger.png" headerIronAlt="trigger icon" headerText="Trigger" bodyText="Convo started"/>
+  <Handle type="target" :position="Position.Bottom" />
+</template>
+
+<style scoped></style>
