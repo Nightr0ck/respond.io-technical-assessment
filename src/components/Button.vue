2e9879122b4f442ae31d9864e1b53e53aff8e671
Nightr0ck <leequan2000@gmail.com>
2024-11-26 00:32:20 +0800

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+    default: "primary",
+  },
+});
+
+const buttonClass = computed(() => {
+  if (["secondary", "destructive"].includes(props.type)) {
+    return [props.type];
+  } else {
+    return ["primary"];
+  }
+});
+
+</script>
+
+<template>
+  <button :class="buttonClass">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  min-width: 80px;
+  height: 35px;
+  border: 0;
+  border-radius: 5px;
+  font-size: medium;
+}
+
+button.primary {
+  background-color: lightblue;
+}
+
+button.secondary {
+  background-color: lightgray;
+}
+
+button.destructive {
+  background-color: red;
+  color: white;
+}
+</style>
